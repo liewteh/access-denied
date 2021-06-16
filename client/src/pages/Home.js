1af2cb4e185e1700362300d846fe010a3b13bc3d
jon <jonprosser84@gmail.com>
2021-06-16 19:13:46 +0100

    made login template

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,35 +1,44 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
 import "./Home.css";
-import logo from "./logo.svg";
+
 
 export function Home() {
-	const [message, setMessage] = useState("Loading...");
 
-	useEffect(() => {
-		fetch("/api")
-			.then((res) => {
-				if (!res.ok) {
-					throw new Error(res.statusText);
-				}
-				return res.json();
-			})
-			.then((body) => {
-				setMessage(body.message);
-			})
-			.catch((err) => {
-				console.error(err);
-			});
-	}, []);
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+
+	
+	
 
 	return (
 		<main role="main">
-			<div>
-				<img className="logo" data-qa="logo" src={logo} alt="Just the React logo" />
-				<h1 className="message" data-qa="message">{message}</h1>
-				<Link to="/about/this/site">About</Link>
+		<div className="container">
+			<div className="heading">
+			<img
+            className="logo"
+            src="https://syllabus.codeyourfuture.io/img/logo.png"
+            alt="cyf_logo"
+            />
+		  	</div>
+			<div className="title">
+				<h1 className="message">Class Register</h1>
+				<h2>Welcome</h2>
+				<h3>Please Sign In</h3>
 			</div>
+		     <div className="login">
+			 	
+				 <input onChange={(e) => setUsername(e.target.value)} placeholder="username" type="text"></input> 
+			 </div>
+			 <div className="login">
+			     <input onChange={(e) => setPassword(e.target.value)} placeholder="password" type="password"></input>
+			 </div>
+
+				<Link to="/about/this/site">
+				<button  className="login-btn">Sign In</button>
+				</Link>
+		</div>
+
 		</main>
 	);
 }
