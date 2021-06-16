d76823d9a5ca2518c30a4796bee63e6a36200584
Nirmeet <nirmeet.baweja@gmail.com>
2021-06-16 09:44:24 +0100

    installed knex

---
+++ server/knexfile.js
@@ -0,0 +1,32 @@
+// Update with your config settings.
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    client: "postgresql",
+    connection: {
+      user: process.env.DB_USER,
+      database: "cyf",
+      password: process.env.DB_PASS,
+      port: 5432,
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      connectionTimeoutMillis: 5000,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};
