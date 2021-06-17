ddfeb67e59d3c38d1288298cc301e2bec5dec384
jon <jonprosser84@gmail.com>
2021-06-17 19:38:44 +0100

    component folder moved to source

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import Footer from '../../components/Footer';
+import Footer from '../components/Footer';
 import "./Home.css";
 
 
