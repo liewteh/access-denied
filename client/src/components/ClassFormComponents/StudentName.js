4b0a44e302be989aba1af8eddbf58bc9d23300e5
LT <liewtteh@gmail.com>
2021-06-18 19:06:15 +0100

    merge again

--- client/src/components/ClassFormComponents/StudentName.js
+++ client/src/components/ClassFormComponents/StudentName.js
@@ -13,7 +13,7 @@ const StudentName = ({firstName, lastName}) => {
           </div>
           <div className="grid-item">
             <ToggleButtonOff className="ToggleButtonOffContainer" />
-          </div>
+          </div>gi
           <div className="grid-item">
             <input className="lateInput" type="integer" placeholder="minutes" />
           </div>
