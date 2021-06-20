81f3f51860072907e113611018e268005ffc0f9c
LT <liewtteh@gmail.com>
2021-06-20 16:06:45 +0100

    css toggle

--- client/src/components/ClassFormComponents/ToggleButtonOn.js
+++ client/src/components/ClassFormComponents/ToggleButtonOn.js
@@ -17,13 +17,13 @@ const IOSSwitch = withStyles((theme) => ({
       transform: "translateX(16px)",
       color: theme.palette.common.white,
       "& + $track": {
-        backgroundColor: "#52d869",
+        backgroundColor: "#ff2a13",
         opacity: 1,
         border: "none",
       },
     },
     "&$focusVisible $thumb": {
-      color: "#52d869",
+      color: "#ff2a13",
       border: "6px solid #fff",
     },
   },
