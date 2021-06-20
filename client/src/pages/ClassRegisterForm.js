c6702e984c692101b69b35aa46f3339ca8a23924
LT <liewtteh@gmail.com>
2021-06-20 16:09:10 +0100

    add submit button

--- client/src/pages/ClassRegisterForm.js
+++ client/src/pages/ClassRegisterForm.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import Header from '../components/ClassFormComponents/Header.js';
-import StudentName from '../components/ClassFormComponents/StudentName.js';
-import Footer from '../components/Footer';
+import React from "react";
+import Header from "../components/ClassFormComponents/Header.js";
+import StudentName from "../components/ClassFormComponents/StudentName.js";
+import Footer from "../components/Footer";
 import StudentNames from "../TestData/Names.json";
 
-
-import './ClassRegisterForm.css';
+import "./ClassRegisterForm.css";
 
 const ClassRegisterForm = () => {
-    return (
-      <div className="formContainer">
-        <Header />
-        <div className="titleGridContainer">
-          <div className="grid-item">Student Name</div>
-          <div className="grid-item">Absence</div>
-          <div className="grid-item">Late</div>
-          <div className="grid-item">Distract/Not Participate</div>
-          <div className="grid-item">Camera On/Off</div>
-          <div className="grid-item">Tech Issues</div>
-          <div className="grid-item">Comments</div>
-        </div>
+  return (
+    <div className="formContainer">
+      <Header />
+      <div className="titleGridContainer">
+        <div className="grid-item">Student Name</div>
+        <div className="grid-item">Absence</div>
+        <div className="grid-item">Late</div>
+        <div className="grid-item">Distract/Not Participate</div>
+        <div className="grid-item">Camera On/Off</div>
+        <div className="grid-item">Tech Issues</div>
+        <div className="grid-item">Comments</div>
+      </div>
+      <form>
         <div className="studentNameContainer">
           {StudentNames.map((student, index) => (
             <StudentName
@@ -29,11 +29,15 @@ const ClassRegisterForm = () => {
             />
           ))}
         </div>
-        <div className="footer-component">
-          <Footer />
-        </div>
+        <button type="submit" className="submitButton" onClick={submitHandler}>
+          Submit
+        </button>
+      </form>
+      <div className="footer-component">
+        <Footer />
       </div>
-    );
-}
+    </div>
+  );
+};
 
-export default ClassRegisterForm
+export default ClassRegisterForm;
