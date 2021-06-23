7eb642dae893ef4d1f0a6873b3677b65d1760cad
LT <liewtteh@gmail.com>
2021-06-23 12:17:31 +0100

    change fetch to axios

--- client/src/pages/ClassRegisterForm.js
+++ client/src/pages/ClassRegisterForm.js
@@ -1,5 +1,6 @@
 import "./ClassRegisterForm.css";
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Header from "../components/ClassFormComponents/Header.js";
 import StudentName from "../components/ClassFormComponents/StudentName.js";
 import Footer from "../components/Footer";
@@ -14,57 +15,50 @@ const ClassRegisterForm = () => {
   };
 
   const [students, setStudents] = useState([]);
-  
+
+  // get region's class's students data from database
   useEffect(() => {
-      fetch(`/api/students`)
+    axios
+      .get(`/api/students`)
       .then((res) => {
-          if (res.ok) {
-              return res.json()
-          } else {
-              alert(res.status)
-              alert("Error! Data not found")
-          }
-      })
-      .then((data) => {
-          setStudents(data)
+        setStudents(res.data);
       })
       .catch((error) => {
-          console.error("Error while fetching data");
-      })
-  }, [])
+        console.error(`Error while fetching data. \n${error} `);
+      });
+  }, []);
 
   return (
     <div className="formContainer">
       <div className="HeaderContainer">
         <Header />
-      </div> 
+      </div>
       <div className="classTitle">
         <RegionAndClassTitle />
         <DownloadReportButton className="DownloadReportButton" />
-      </div> 
+      </div>
       <div className="titleGridContainer">
-        <div className="grid-item"> Student Name </div> 
-        <div className="grid-item"> Absence </div> 
-        <div className="grid-item"> Late </div> 
-        <div className="grid-item"> Distract / Not Participate </div> 
-        <div className="grid-item"> Camera On / Off </div> 
-        <div className="grid-item"> Tech Issues </div> 
-        <div className="grid-item"> Comments </div> 
-      </div> 
+        <div className="grid-item"> Student Name </div>
+        <div className="grid-item"> Absence </div>
+        <div className="grid-item"> Late </div>
+        <div className="grid-item"> Distract / Not Participate </div>
+        <div className="grid-item"> Camera On / Off </div>
+        <div className="grid-item"> Tech Issues </div>
+        <div className="grid-item"> Comments </div>
+      </div>
       <form>
         <div className="studentNameContainer">
-           
           {students.map((student, index) => (
             <StudentName studentName={student.user_name} key={index} />
-          ))} 
-        </div> 
+          ))}
+        </div>
         <button type="submit" className="submitButton" onClick={submitHandler}>
-          Submit 
-        </button> 
-      </form> 
+          Submit
+        </button>
+      </form>
       <div className="footer-component">
         <Footer />
-      </div> 
+      </div>
     </div>
   );
 };
