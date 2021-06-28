66ded7f2559cdd9f02d6ff814d26fe37a109bf64
Nirmeet <nirmeet.baweja@gmail.com>
2021-06-28 22:26:39 +0100

    removed promise error handling because not compatible with axios.get
    error should be handled with catch() from axios.

--- client/src/pages/Cohorts.js
+++ client/src/pages/Cohorts.js
@@ -17,15 +17,16 @@ const Cohorts = () => {
       console.log(response);
       const cohorts = response.data;
       console.log(cohorts);
-      if (!response.ok) {
-        const message = `An error has occurred: ${response.status}`;
-        throw new Error(message);
-      }
+      // if (!response.ok) {
+      //   const message = `An error has occurred: ${response.status}`;
+      //   throw new Error(message);
+      // }
       setCohortList(cohorts);
+      console.log(cohorts);
     };
     console.log("in useEffect.");
     fetchCohorts();
-  });
+  }, []);
 
   return (
     <div className="cohortPage">
