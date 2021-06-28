c1585726a584faac6b5d59d3cb674e1f1570f6b4
Nirmeet <nirmeet.baweja@gmail.com>
2021-06-28 22:24:43 +0100

    Replaced the div with a <Link> to open cohort classes.

--- client/src/components/CohortCard.js
+++ client/src/components/CohortCard.js
@@ -1,19 +1,11 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./CohortCard.css";
 
 const CohortCard = ({ cohort }) => {
-  const history = useHistory();
-
-  const routeChange = () => {
-    let path = `/cohorts/${cohort.id}/classes`;
-    history.push(path);
-  };
-
-  // const showClasses = () => {};
   return (
     // eslint-disable-next-line jsx-a11y/click-events-have-key-events
-    <div className="infoCard" onClick={routeChange} role="link" tabIndex={0}>
+    <div className="infoCard" tabIndex={0}>
       <div className="cohortDetails">
         <h2 className="cohortName">
           {`${cohort.region_name} Class ${cohort.cohort_number}`}
@@ -28,6 +20,7 @@ const CohortCard = ({ cohort }) => {
             {cohort.cohort_number}
           </li>
         </ul>
+        <Link to={`/cohorts/${cohort.id}/classes`}>Go to classes</Link>
       </div>
     </div>
   );
