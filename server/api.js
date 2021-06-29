a72b5ba6f6ee5ae8870c9c14916e2a0a6a5a4a34
LT <liewtteh@gmail.com>
2021-06-29 16:24:05 +0100

    Student-Attendance API Done and Tested

--- server/api.js
+++ server/api.js
@@ -16,4 +16,24 @@ router.get("/students", async (req, res) => {
   res.send(students);
 });
 
+router.get(
+  "/cohorts/:cohortId/classes/:classId/students-attendance",
+  async (req, res) => {
+    const cohortId = req.params.cohortId;
+    const classId = req.params.classId;
+
+    // get student list of a region's name, and its class's number
+    const students = await knex("cohorts as c")
+      .select("*")
+      .join("regions as r", "r.id", "c.region_id")
+      .join("cohort_members as cm", "c.id", "cm.cohort_id")
+      .join("users as u", "u.id", "cm.user_id")
+      .join("class_attendances as ca", "ca.user_id", "u.id")
+      .where("u.user_name", "like", "Student%")
+      .andWhere("c.id", cohortId)
+      .andWhere("ca.class_id", classId);
+    res.send(students);
+  }
+);
+
 export default router;
