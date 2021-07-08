a8f09b0dac276e52af74d372f62e67a33e7a6720
LT <liewtteh@gmail.com>
2021-07-08 19:07:45 +0100

    remove unused code

--- client/src/pages/Welcome.js
+++ client/src/pages/Welcome.js
@@ -1,50 +1,29 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./Welcome.css";
 
 const Welcome = () => {
-  const history = useHistory();
 
   // TODO selectedUser - to hide/display options
-  let selectedUser = "Admin";
-  let createUserOption, cohortsOption;
-
-  if (selectedUser === "Admin") {
-    createUserOption = "showOption";
-    cohortsOption = "showOption";
-  } else if (selectedUser === "Teacher") {
-    createUserOption = "hideOption";
-    cohortsOption = "showOption";
-  } else if (selectedUser === "Student") {
-    createUserOption = "hideOption";
-    cohortsOption = "hideOption";
-  }
+  let isAdmin = true;
 
   //  TODO goto Create User page
   const routeChangeCreateUser = () => {
     console.log("createUser clicked!");
   };
 
-  //   goto cohorts page
-  const routeChangeCohorts = () => {
-    let path = `/cohorts`;
-    history.push(path);
-  };
-
   return (
     <div className="welcomePage">
-      <h1 className="welcomePageHeading">Welcome {selectedUser}</h1>
+      <h1 className="welcomePageHeading">Welcome To CYF Register</h1>
       <div className="optionsContainer">
-        <h2
-          className={createUserOption}
-          onClick={routeChangeCreateUser}
-          role="link"
-        >
-          Create User
-        </h2>
-        <h2 className={cohortsOption} onClick={routeChangeCohorts} role="link">
-          Cohorts
-        </h2>
+        {isAdmin && (
+          <div>
+            <Link to="/create-user">Create User</Link>
+          </div>
+        )}
+        <div>
+          <Link to="/cohorts">Cohorts</Link>
+        </div>
       </div>
     </div>
   );
